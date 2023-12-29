1c79f0359bea5c1384ddf1da312a82de2b1fa819
Harbour-N <mbyneh@nottingham.ac.uk>
2023-12-29 13:19:52 +0000

    initalised a simple R shiny app environment

---
+++ app.py
@@ -0,0 +1,15 @@
+from shiny import App, ui
+
+# Part 1: ui ----
+app_ui = ui.page_fluid(
+    ui.input_slider("n", "Choose a number n:", 0, 100, 40 ),
+    ui.output_text_verbatim("txt")
+)
+
+# Part 2: server ----
+def server(input, output, session):
+    ...
+
+# Combine into a shiny app.
+# Note that the variable must be "app".
+app = App(app_ui, server)
